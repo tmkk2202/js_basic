e32639913a05dc6572061d934f360017a1c749ca
Thetmon22 <22thetmon@gmail.com>
2022-11-30 16:38:22 +0630

    git init

---
+++ vscode shortcut.js
@@ -0,0 +1,26 @@
+// vscode shortcut
+// 1. C + B	            left side bar
+// 2. C + `	            Built in terminal
+// 3. C + ,	            Setting
+// 4. C + P	            Open file
+// 5. C + Tab	        Tab through open file
+// 6. C + \	            Split editor
+// 7. C + W	            Close open file
+// 8. C + F	            Search/Replace in file
+// 9. C+S + F	        Global Search/Replace
+// 10.C+S + P	        Open command palette
+// 11.C + /	            Toggle Comment a Line(create command line for javascript)
+// 12.A + Arrow	        Navigating Text faster
+// 13.C+ C/V/X	        copy,paste,cut (no need to select whole line in vscode)
+// 14.S+A+UP/Down	    Duplicate line
+// 15.A + Click	        Multile Cursors
+// 16.C + D	            Select Word
+// 17.C+S+L	            Select all instances of a word(Select all same words in the sheet)
+// 18.F2		        Rename symbol
+// 19.C+Click	        jump to function definition
+// 20.C+KZ		        zen mode
+// 21.C+A+L/R	        compare view of parallel open file/sheet
+// 22.C+S+Tab	        tab throung open file/sheeet
+// 23.W+UpDown/LR       minimize/maximize open file
+// 24.A + Tab	        tab through open software
+// 25.S + Tab	        once step backward of tab move
